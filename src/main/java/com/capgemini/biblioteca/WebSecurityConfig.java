26758e2bbafdc2e0ae4564ffae8a72a05b1c16fb
Ferreira Gomez <daniel.ferreira-gomez@capgemini.com>
2023-10-26 17:14:35 +0200

    thymeleaf sec funcionando

--- src/main/java/com/capgemini/biblioteca/WebSecurityConfig.java
+++ src/main/java/com/capgemini/biblioteca/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 import com.capgemini.biblioteca.services.UserDetailsServiceImpl;
  
