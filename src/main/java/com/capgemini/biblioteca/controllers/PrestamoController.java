a37efee39a685644dfe6c7c305c7b2b27ae64f47
Ferreira Gomez <daniel.ferreira-gomez@capgemini.com>
2023-10-27 11:33:21 +0200

    Ver prestamos por parte del lector hecho

--- src/main/java/com/capgemini/biblioteca/controllers/PrestamoController.java
+++ src/main/java/com/capgemini/biblioteca/controllers/PrestamoController.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,18 +15,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.capgemini.biblioteca.model.Prestamo;
+import com.capgemini.biblioteca.model.Usuario;
 import com.capgemini.biblioteca.services.PrestamoService;
+import com.capgemini.biblioteca.services.UsuarioService;
 
 @Controller
 public class PrestamoController {
 
 	@Autowired
 	private PrestamoService prestamoService;
+	
+	@Autowired
+	private UsuarioService usuarioService;
 
-	@GetMapping("/prestamos/{id}") // http://localhost:8080/users/id/2 se invocara asi
-	public String getPrestamoById(@PathVariable("id") long id) {
-		Prestamo prestamo = prestamoService.getEntityById(id);
-		return "detalles_prestamo";
+	@GetMapping("/prestamos/{lector_id}")
+	public String getPrestamoByLectorId(@PathVariable("lector_id") long lector_id, Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Usuario user = this.usuarioService.getUserByUsername(auth.getName());
+		if (user.getId() != lector_id) {
+			return "error";
+		}
+		List<Prestamo> prestamos = this.prestamoService.findByLectorId(lector_id);
+		model.addAttribute("prestamos", prestamos);
+		return "listaPrestamos";
 	}
 
 	@GetMapping("/prestamos")
@@ -40,7 +53,7 @@ public class PrestamoController {
 			@ModelAttribute("libro_id") long libro_id,
 			Model model) {
 		p.setInicio(new Date());
-		
+		System.out.println("hoddsjlfsdka");
 		if (p.getInicio().after(p.getFin())) {
 			return "error";
 		}else {
--- src/main/java/com/capgemini/biblioteca/controllers/UserController.java
+++ src/main/java/com/capgemini/biblioteca/controllers/UserController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.capgemini.biblioteca.model.Lector;
 import com.capgemini.biblioteca.model.Usuario;
+import com.capgemini.biblioteca.services.LectorService;
 import com.capgemini.biblioteca.services.RolesService;
 import com.capgemini.biblioteca.services.SecurityService;
 import com.capgemini.biblioteca.services.UsuarioService;
@@ -19,6 +22,9 @@ public class UserController {
 	
 	@Autowired
     private UsuarioService usersService;
+	
+	@Autowired
+	private LectorService lectorService;
 
     @Autowired
     private SecurityService securityService;
@@ -42,12 +48,16 @@ public class UserController {
 	
 	@PostMapping("/signup")
     public String signup(@Validated Usuario user, BindingResult result) {
-		System.out.println("Hola");
         signUpFormValidator.validate(user, result);
-        if (result.hasErrors())
+        if (result.hasErrors()) {
             return "signup";
-        System.out.println("Hola2");
+        }
+        Lector lector = new Lector();
+        lector.setNombre(user.getName());
+        lector.setDireccion(user.getDireccion());
         user.setRole(rolesService.getRoles()[0]);
+        user.setLector(lector);
+        lectorService.saveEntity(lector);
         usersService.saveEntity(user);
         securityService.autoLogin(user.getUsername(), user.getPasswordConfirm());
         return "redirect:/";
